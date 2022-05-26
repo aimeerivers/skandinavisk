b5bd77cceddf2476d97a5e1e53bb84401877503a
Aimee Rivers <airi@dr.dk>
2022-05-26 23:27:44 +0200

    Translate to da/no/sv

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('public'));
+
+const { TranslationServiceClient } = require('@google-cloud/translate');
+const { text } = require('express');
+const translationClient = new TranslationServiceClient();
+
+const projectId = 'sjov-oversaetter-1531150037182';
+const location = 'global';
+
+app.get('/', async (req, res) => {
+  var text = 'A shared Scandinavian language';
+  if (req.query.q) text = req.query.q;
+  res.render('home', {
+    title: 'Skandinavisk',
+    text: text,
+    da: await translateText(text, 'en', 'da'),
+    no: await translateText(text, 'en', 'no'),
+    sv: await translateText(text, 'en', 'sv'),
+  });
+});
+
+async function translateText(text, source, target) {
+  const request = {
+    parent: `projects/${projectId}/locations/${location}`,
+    contents: [text],
+    mimeType: 'text/plain',
+    sourceLanguageCode: source,
+    targetLanguageCode: target,
+  };
+
+  const [response] = await translationClient.translateText(request);
+  return response.translations[0].translatedText;
+}
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
